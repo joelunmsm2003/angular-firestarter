26c1dedede424eb1d7d244ee09a28bb95826b9cc
root <root@Codigito.codigito>
2018-09-19 00:46:30 -0500

    c

--- lanza.sh
+++ lanza.sh
@@ -1,7 +1,9 @@
-cd /Users/xiencias/code/angular-firestarter
+cd /home/andy/fire
+nvm install 9
+ng build
 rm -rf dist.tar.gz
 tar -czvf dist.tar.gz dist
-scp -P 5022 dist.tar.gz root@xiencias.com:/var/www/html/
+scp -P 5022 dist.tar.gz root@xiencias.com:/var/www/
 ssh root@xiencias.com -p 5022
 cd /var/www/html
 tar -xzvf dist.tar.gz
